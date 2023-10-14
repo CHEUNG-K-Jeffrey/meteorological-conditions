36b78d3ec5f4e9bd2d2b1814cede41e1ec2a2ce8
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-10-13 18:33:11 -0700

    Add initial implementation of HomePage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,46 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+
+function Location() {
+  return (
+    <>
+        <div>Location</div>
+        <input type="text" placeholder="Location" value="San Francisco, Ca" disabled></input>
+    </>
+  )
+}
+
+function Clock() {
+  const [time, setTime] = useState();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((new Date()).getTime())
+    }, 1000);
+  
+    return () => clearInterval(interval);
+  }, []);
+  
+  return (
+    <div>{time}</div>
+  )
+}
+
+
+function HomePage() {
+  return (
+    <div id="home-page" className="p-20">
+      <Location></Location>
+      <Clock></Clock>
+    </div>
+  )
+}
+
+
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col h-screen">
+      <HomePage></HomePage>
     </main>
   )
 }
