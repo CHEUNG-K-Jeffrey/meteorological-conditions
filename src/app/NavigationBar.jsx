2de3fe3842051112b670a577b4b91d9be50cedf3
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-10-22 12:24:06 -0700

    Make navigation bar responsive and bigger

--- src/app/NavigationBar.jsx
+++ src/app/NavigationBar.jsx
@@ -1,6 +1,6 @@
 
 export default function NavigationBar() {
     return (
-        <div className="w-screen h-20 bg-black"></div>
+        <div className="w-screen h-40 sm:h-28 bg-black"></div>
     );
 }
