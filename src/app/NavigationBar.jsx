7f7c0f2d6cdc38eb874c77ecb6a086293057483f
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-10-22 13:07:37 -0700

    Make NavBar stay at the bottom of the screen

--- src/app/NavigationBar.jsx
+++ src/app/NavigationBar.jsx
@@ -1,7 +1,7 @@
 
 export default function NavigationBar() {
     return (
-        <div className="flex w-screen h-24  bg-black">
+        <div className="self-end flex w-screen h-24  bg-black">
             <div className="w-24 bg-slate-100">Home</div>
             <div className="w-24 bg-slate-100">Temperature</div>
         </div>
