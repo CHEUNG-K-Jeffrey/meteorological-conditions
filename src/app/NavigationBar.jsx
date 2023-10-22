5780892f9e9365bcad26a87090fd8f4f51a3ac9e
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-10-22 12:39:18 -0700

    Adjust height of bar and width of each button

--- src/app/NavigationBar.jsx
+++ src/app/NavigationBar.jsx
@@ -1,9 +1,9 @@
 
 export default function NavigationBar() {
     return (
-        <div className="flex w-screen h-40 sm:h-28 bg-black">
-            <div className="w-40 sm:w-28 bg-slate-100">Home</div>
-            <div className="w-40 sm:w-28 bg-slate-100">Temperature</div>
+        <div className="flex w-screen h-24  bg-black">
+            <div className="w-24 bg-slate-100">Home</div>
+            <div className="w-24 bg-slate-100">Temperature</div>
         </div>
     );
 }
