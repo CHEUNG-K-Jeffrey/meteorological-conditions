2d5562e17f3e1ae3fd28c0f74e09268fc181f4e4
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-10-22 12:30:01 -0700

    Add buttons on navbar

--- src/app/NavigationBar.jsx
+++ src/app/NavigationBar.jsx
@@ -1,6 +1,9 @@
 
 export default function NavigationBar() {
     return (
-        <div className="w-screen h-40 sm:h-28 bg-black"></div>
+        <div className="flex w-screen h-40 sm:h-28 bg-black">
+            <div className="w-40 sm:w-28 bg-slate-100">Home</div>
+            <div className="w-40 sm:w-28 bg-slate-100">Temperature</div>
+        </div>
     );
 }
