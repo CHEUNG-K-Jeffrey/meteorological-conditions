f57080a8b3844c1d2f479ccb987444d8e41555a3
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-10-27 15:39:55 -0700

    Add clickable home button with icon

--- src/app/NavigationBar.jsx
+++ src/app/NavigationBar.jsx
@@ -2,7 +2,7 @@
 export default function NavigationBar() {
     return (
         <div className="self-end flex justify-evenly w-screen h-24  bg-slate-300">
-            <div className="w-24 bg-slate-200">Home</div>
+            <a className=" bg-slate-200 hover:cursor-pointer w-24 flex justify-center items-center" href="/"><i className="fa-4x fa-solid fa-house"></i></a>
             <div className="w-24 bg-slate-200">Temperature</div>
         </div>
     );
