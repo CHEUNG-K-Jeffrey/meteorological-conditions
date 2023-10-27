2c167084786cdba00fce705ceb77fa96c54d21fe
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-10-27 14:46:37 -0700

    Adjust the color and spacing of navbar

--- src/app/NavigationBar.jsx
+++ src/app/NavigationBar.jsx
@@ -1,7 +1,7 @@
 
 export default function NavigationBar() {
     return (
-        <div className="self-end flex w-screen h-24  bg-black">
+        <div className="self-end flex justify-evenly w-screen h-24  bg-slate-300">
             <div className="w-24 bg-slate-100">Home</div>
             <div className="w-24 bg-slate-100">Temperature</div>
         </div>
