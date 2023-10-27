a8780ebb3e0725b40b441bee1b6ca9e803cfe96b
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-10-27 15:58:12 -0700

    Refactor NavigationBar to use NavigationButton

--- src/app/NavigationBar.jsx
+++ src/app/NavigationBar.jsx
@@ -1,9 +1,20 @@
+function NavigationButton(props) {
+    return (
+        <a className="bg-slate-200 hover:cursor-pointer w-24 flex justify-center items-center" href={props.href}>
+            {props.children}
+        </a>
+    )
+}
 
 export default function NavigationBar() {
     return (
         <div className="self-end flex justify-evenly w-screen h-24  bg-slate-300">
-            <a className=" bg-slate-200 hover:cursor-pointer w-24 flex justify-center items-center" href="/"><i className="fa-4x fa-solid fa-house"></i></a>
-            <div className="w-24 bg-slate-200">Temperature</div>
+            <NavigationButton href="/">
+                <i className="fa-4x fa-solid fa-house"></i>
+            </NavigationButton>
+            <NavigationButton href="/Temperature">
+                <i className="fa-4x fa-solid fa-temperature-half"></i>
+            </NavigationButton>
         </div>
     );
 }
