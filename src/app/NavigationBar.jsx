eede34a64c25c924317261d4a5d0be854ca5f39f
Jeffrey Cheung <139647007+CHEUNG-K-Jeffrey@users.noreply.github.com>
2023-10-27 16:21:07 -0700

    Make NavigationBar responsive

--- src/app/NavigationBar.jsx
+++ src/app/NavigationBar.jsx
@@ -1,6 +1,6 @@
 function NavigationButton(props) {
     return (
-        <a className="bg-slate-200 hover:cursor-pointer w-24 flex justify-center items-center" href={props.href}>
+        <a className="bg-slate-200 hover:cursor-pointer w-20 sm:w-24 flex justify-center items-center" href={props.href}>
             {props.children}
         </a>
     )
@@ -8,12 +8,12 @@ function NavigationButton(props) {
 
 export default function NavigationBar() {
     return (
-        <div className="self-end flex justify-evenly w-screen h-24  bg-slate-300">
+        <div className="self-end flex justify-evenly w-screen h-20 sm:h-24  bg-slate-300">
             <NavigationButton id="home-page-button" href="/">
-                <i className="fa-4x fa-solid fa-house"></i>
+                <i className="fa-3x fa-solid fa-house"></i>
             </NavigationButton>
             <NavigationButton id="temperature-page-button" href="/Temperature">
-                <i className="fa-4x fa-solid fa-temperature-half"></i>
+                <i className="fa-3x fa-solid fa-temperature-half"></i>
             </NavigationButton>
         </div>
     );
